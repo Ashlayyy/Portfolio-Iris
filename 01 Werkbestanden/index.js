c5cb65e8254ac5f04397171a7a72df5b95d36c32
Ashlay <105703068+AshlayMeijer@users.noreply.github.com>
2022-12-08 21:02:26 +0100

    Added a space to test out

--- 01 Werkbestanden/index.js
+++ 01 Werkbestanden/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const favicon = require('serve-favicon');
-const app = express();
+const app = express(); 
 const port = 8080;
 const bodyParser = require('body-parser');
 const compression = require('compression');
