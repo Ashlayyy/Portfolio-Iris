bddf13f985ffe7bc388a422803f570d1505243fb
Ashlay <105703068+AshlayMeijer@users.noreply.github.com>
2022-12-16 17:04:16 +0100

    af

--- 01-Werkbestanden/index.js
+++ 01-Werkbestanden/index.js
@@ -15,7 +15,7 @@ app.use('/', require('./Routes/index'));
 app.use('/css', express.static('Public/css'));
 app.use('/js', express.static('Public/js'));
 app.use('/html', express.static('Public/html'));
-
+ 
 app.use('/download', express.static('Public/download'))
 
 app.use('/img', express.static("../04-Archief/01 Foto's"));
