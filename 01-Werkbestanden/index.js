403ae6fd96f4d252598505940ed6f3ae53fc9ac5
Ashlay <105703068+AshlayMeijer@users.noreply.github.com>
2023-04-11 11:58:46 +0200

    Fix javascript files

--- 01-Werkbestanden/index.js
+++ 01-Werkbestanden/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 const express = require('express')
 const favicon = require('serve-favicon');
 const app = express(); 
-const port = 8080;
+const port = process.env.PORT;
 const bodyParser = require('body-parser'); 
 const compression = require('compression');
  
@@ -10,17 +12,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
 
-app.use('/', require('./Routes/index.new'));
+app.use('/', require('./Routes/index'));
 
 app.use('/css', express.static('Public/css')); 
 app.use('/js', express.static('Public/js'));
 app.use('/html', express.static('Public/html'));
 app.use('/components', express.static('Public/components'));
- 
-app.use('/download', express.static('Public/download'))
-
+app.use('/download', express.static('Public/download'));
 app.use('/img', express.static("../04-Archief/01 Foto's"));
-
 
 app.use(favicon(__dirname + '/favicon.ico'));
 
